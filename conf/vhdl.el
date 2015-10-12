287e3d338f1a2defb3ff83fd5909f9ad2bcb1d10
Steffen Roskamp <steffen.roskamp@gmail.com>
2015-10-12 20:50:39 +0200

    Disabled VHDL direct instantiation

--- conf/vhdl.el
+++ conf/vhdl.el
@@ -1,6 +1,10 @@
 (setq vhdl-date-format "%d.%m.%Y")
+
 (setq vhdl-electric-mode t)
 (setq vhdl-stutter-mode t)
+
 (setq vhdl-upper-case-enum-values t)
+
+(setq vhdl-use-direct-instantiation nil)
 
 (provide 'vhdl)
