03806f7fb26c5235fcc2cb04d4c091704c8f5761
Steffen Roskamp <steffen.roskamp@gmail.com>
2015-11-30 11:55:07 +0100

    Some style changes to vhdl.el

--- conf/vhdl.el
+++ conf/vhdl.el
@@ -5,9 +5,15 @@
 
 (setq vhdl-upper-case-enum-values t)
 
+; Template settings
 (setq vhdl-use-direct-instantiation nil)
 (setq vhdl-clock-edge-condition (quote function))
 (setq vhdl-underscore-is-part-of-word t)
 
+; Sensitivity list
+(setq vhdl-array-index-record-field-in-sensitivity-list nil)
+
+; others
+(setq vhdl-underscore-is-part-of-word t)
 
 (provide 'vhdl)
