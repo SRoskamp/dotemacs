a0c3b45c2eeca49937b5d3e9cb05307aeca896e5
Steffen Roskamp <steffen.roskamp@gmail.com>
2016-08-24 09:50:48 +0200

    Added IMS-specific changes

--- init.el
+++ init.el
@@ -1,7 +1,10 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu"   . "http://elpa.gnu.org/packages/"))
 (package-initialize)
+
+; Change meta-key (IMS only)
+(setq x-super-keysym 'meta)
 
 (org-babel-load-file "~/.emacs.d/config.org")
